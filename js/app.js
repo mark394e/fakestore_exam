b1ab276407de1b0986bc470fc7f4da67816345e3
Markus <mark394e@stud.kea.dk>
2023-11-14 17:28:50 +0100

    error message password

--- js/app.js
+++ js/app.js
@@ -3,12 +3,12 @@
 //document.querySelector('.cta.sign-up-btn').addEventListener('click', initiate);
 document.querySelector(".sign-up-form").addEventListener("submit", function (event) {
   event.preventDefault();
-  validate();
+  validate_sign_up();
 });
 
 //################################################
 
-function validate() {
+function validate_sign_up() {
   const user_password = document.getElementById("password").value;
   const passwordRegEx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+]).{8,20}$/;
   const isValidPassword = passwordRegEx.test(user_password);
@@ -21,20 +21,27 @@ function validate() {
   const isValidConfirmPassword = user_password === user_confirm_password;
 
   if (!isValidPassword) {
-    const password_input_field = document.getElementById("password_login");
-    const error_message = document.createElement("p");
-    password_input_field.appendChild(error_message);
-    alert(
-      "Password must be between 8 to 20 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character."
+    const password_input_field = document.getElementById("password");
+    const error_message_element = password_input_field.insertAdjacentElement(
+      "afterend",
+      document.createElement("p")
     );
+    error_message_element.classList.add("error-message");
+    error_message_element.innerHTML =
+      "Password must be between 8 to 20 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character.";
+    password_input_field.style.border = "1px solid var(--danger_color)";
+    password_input_field.focus();
+    return;
   }
 
   if (!isValidEmail) {
     alert("Please enter a valid email address.");
+    return;
   }
 
   if (!isValidConfirmPassword) {
     alert("Passwords do not match.");
+    return;
   }
 
   console.log(isValidPassword);
