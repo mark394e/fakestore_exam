e3e01fd46f067d9fd7079f11baf6d35212792485
Markus <mark394e@stud.kea.dk>
2023-11-22 12:22:28 +0100

    a bunch of shit

--- js/checkout.js
+++ js/checkout.js
@@ -11,65 +11,67 @@ const zipcode_error_message = document.querySelector("#zipcode-error");
 const card_number_input_field = document.querySelector("#card-number");
 const card_number_error_message = document.querySelector("#card-number-error");
 
-cvv_input_field.addEventListener("input", function (e) {
+cvv_input_field.addEventListener("change", function (e) {
   let input = e.target.value;
+  if (input.length === 0) {
+    cvv_input_field.classList.remove("success-border");
+  }
   validate_cvv_input(input);
 });
 
 expiry_input_field.addEventListener("input", function (e) {
   let input = e.target.value;
+  insert_slash(input);
+});
+
+expiry_input_field.addEventListener("change", function (e) {
+  let input = e.target.value;
+  if (input.length === 0) {
+    expiry_input_field.classList.remove("success-border");
+  }
   validate_expiry(input);
 });
 
 function insert_slash(input) {
-  if (input.length > 2) {
-    console.log("insert slash");
-    input = input.slice(0, 2) + "/" + input.slice(2, 4);
-    expiry_error_message.classList.add("hidden");
-    expiry_input_field.classList.remove("error-border");
-    expiry_input_field.classList.add("success-border");
+  if (input.length >= 2) {
+    input = input.slice(0, 2) + "/" + input.slice(2);
   }
+  expiry_input_field.value = input;
 }
 
 function validate_cvv_input(input) {
   const cvv_RegEx = /^[0-9]+$/;
   let isValidCvv = cvv_RegEx.test(input);
+
   if (!isValidCvv && input.length != 0) {
-    console.log("Invalid cvv");
-    input = input.replace(/[^a-zA-Z]/g, "");
     cvv_error_message.classList.remove("hidden");
     cvv_input_field.classList.add("error-border");
-    cvv_input_field.classList.remove("success-border");
+    return;
+  }
+  if (input.length != 0) {
+    cvv_input_field.classList.add("success-border");
+
     return;
   }
 
   cvv_error_message.classList.add("hidden");
   cvv_input_field.classList.remove("error-border");
-  cvv_input_field.classList.add("success-border");
   cvv_input_field.value = input;
   return;
 }
 
-function validate_input(input) {
+function validate_expiry(input) {
   const expiry_date_RegEx = /^[0-9\/]+$/;
   let isValidExpiryDate = expiry_date_RegEx.test(input);
 
   if (!isValidExpiryDate && input.length != 0) {
-    console.log("Invalid expiry date");
-    input = input.replace(/[^a-zA-Z]/g, "");
     expiry_error_message.classList.remove("hidden");
     expiry_input_field.classList.add("error-border");
-    // expiry_input_field.value = input;
-    // replace_input_text(input);
     return;
   }
 
-  expiry_input_field.value = input;
-  insert_slash(input);
-}
-
-function replace_input_text(input) {
-  console.log("replace input", input);
-
-  validate_input(input);
+  expiry_error_message.classList.add("hidden");
+  expiry_input_field.classList.remove("error-border");
+  expiry_input_field.classList.add("success-border");
+  return;
 }
