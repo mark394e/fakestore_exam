5e6847ca65cb8283fe4cb25268e4e81b1b6b487d
Anna Maria Lester <anna47c4@stud.kea.dk>
2023-11-28 18:59:51 +0100

    cart and so on 'done'

--- js/script_cart.js
+++ js/script_cart.js
@@ -1,6 +1,6 @@
 "use strict";
-/* CART QUANTITY BUTTONS */
-function cartQuantityButtons() {
+/* CART QUANTITY BUTTONS // Nice to have  */
+/* function cartQuantityButtons() {
   //Empty inputfield from the beginning
   document.querySelector("input.input-group-field").value = 0;
 
@@ -30,4 +30,169 @@ function cartQuantityButtons() {
   }
 }
 
-cartQuantityButtons();
+cartQuantityButtons(); */
+
+/* #### LOCALSTORAGE PRODUCTS ADDED TO CART #### */
+// cart = the 'cart' we get from local storage
+let cart = getCartFromLocalStorage();
+
+// For use in click eventListener later on
+const goToCheckoutButton = document.querySelector("button.checkout-btn-cart");
+
+// We call the updateCart after getting the cart,
+// to check the length and adding/removing the
+// red dot from the cart icon
+updateCart(cart);
+
+// Function that takes care of remowing products
+function removeProductFromCart(productId) {
+  const indexToRemove = cart.findIndex((product) => product.id === productId);
+  if (indexToRemove !== -1) {
+    cart.splice(indexToRemove, 1);
+    updateCart(cart); //call the update function
+  }
+}
+
+// Function that sets the updatedCart, and call showCartProducts again
+function updateCart(updatedCart) {
+  console.log("Updated cart length:", updatedCart.length);
+  localStorage.setItem("cart", JSON.stringify(updatedCart));
+
+  // Check if the cart is empty
+  const cartIsEmpty = updatedCart.length === 0;
+
+  // Find the circle (red dot) in the HTML
+  const redDotOnCart = document.querySelector("circle.red-dot-on-cart");
+  // Add or remove the hidden class
+  if (cartIsEmpty) {
+    redDotOnCart.classList.add("hidden");
+    document.querySelector("button.checkout-btn-cart").classList.add("hidden");
+  } else {
+    redDotOnCart.classList.remove("hidden");
+  }
+
+  // Calculate total and update in HTML
+  const total = calculateTotal(updatedCart);
+  const delivery = calculateDelivery(total);
+
+  updateCalculationInHTML(total, delivery);
+
+  showCartProducts(updatedCart);
+}
+
+// Function that calculates the total
+function calculateTotal(cart) {
+  return cart.reduce((total, product) => total + parseFloat(product.price), 0);
+}
+// Function that calculates the delivery
+function calculateDelivery(total) {
+  if (total === 0) {
+    return 0.0; // If empty cart, return 0.0
+  } else {
+    return total < 20 ? 5.0 : 0.0; // if the total is more than 20, the delivery is 0, else it is 5
+  }
+}
+
+// Show the prices in HTML
+function updateCalculationInHTML(total, delivery) {
+  // Element that show the total
+  const totalPriceElement = document.querySelector("p.order-total-value");
+  totalPriceElement.textContent = total.toFixed(2) + ",-";
+
+  // Element that show the delivery
+  const deliveryPriceElement = document.querySelector("p.order-delivery-value");
+  deliveryPriceElement.textContent = delivery + ",-";
+
+  // Element that shows the subtotal
+  const subtotalPriceElement = document.querySelector("p.order-subtotal-value");
+  subtotalPriceElement.textContent = (total + delivery).toFixed(2) + ",-";
+  /*   console.log(total); */
+}
+
+// The event handler which sets the updatedCart, and calll the updateCart function
+function handleUpdateCartEvent(event) {
+  const updatedCart = event.detail;
+  updateCart(updatedCart);
+}
+
+// Listen for the event in productview.js
+window.addEventListener("updateCartEvent", handleUpdateCartEvent);
+
+// Function where we get the cart and convert it to a JS object
+// and specifies an empty array as default value
+function getCartFromLocalStorage() {
+  return JSON.parse(localStorage.getItem("cart")) || [];
+}
+
+// Function that shows the products in the cart
+function showCartProducts(cart) {
+  const cartContainer = document.querySelector("section.cart-container");
+
+  // Empty the container
+  cartContainer.innerHTML = "";
+
+  // Check if there is products in the cart
+  if (cart.length === 0) {
+    document.querySelector("h1.heading").textContent = "The cart is empty!";
+  } else {
+    cart.forEach((product) => {
+      // Create HTML elements for the products
+      const productDiv = document.createElement("div");
+      productDiv.classList.add("cart-item");
+
+      // Create a wrapper div for item text
+      const imgWrapper = document.createElement("div");
+      imgWrapper.classList.add("item-img-wrapper");
+
+      const img = document.createElement("img");
+      img.src = product.image;
+      img.setAttribute("alt", "Image of the product");
+      img.classList.add("product-in-cart-img");
+      imgWrapper.appendChild(img);
+
+      // Append the wrapper div to the productDiv
+      productDiv.appendChild(imgWrapper);
+
+      // Create a wrapper div for item text
+      const textWrapper = document.createElement("div");
+      textWrapper.classList.add("item-text-wrapper");
+
+      const h2 = document.createElement("h2");
+      h2.textContent = product.title;
+      h2.classList.add("item-name");
+      textWrapper.appendChild(h2);
+
+      const pCategory = document.createElement("p");
+      pCategory.textContent = product.category;
+      pCategory.classList.add("item-category");
+      textWrapper.appendChild(pCategory);
+
+      // Append the wrapper div to the productDiv
+      productDiv.appendChild(textWrapper);
+
+      const pPrice = document.createElement("p");
+      pPrice.textContent = product.price + ",-";
+      pPrice.classList.add("item-price");
+      productDiv.appendChild(pPrice);
+
+      // Delete item button
+      const deleteProductFromCart = document.createElement("button");
+      deleteProductFromCart.textContent = "Remove";
+      deleteProductFromCart.classList.add("delete-item-from-cart-button");
+      productDiv.appendChild(deleteProductFromCart);
+      deleteProductFromCart.addEventListener("click", () =>
+        removeProductFromCart(product.id)
+      );
+
+      // Add the product to the container
+      cartContainer.appendChild(productDiv);
+    });
+  }
+}
+
+// Go to checkout page - event on button
+goToCheckoutButton.addEventListener("click", () => {
+  location.href = "../checkout.html";
+});
+
+showCartProducts(cart);
--- js/script_productview.js
+++ js/script_productview.js
@@ -13,32 +13,55 @@ function isUserLoggedIn() {
   return;
 }
 
-//loader element for ux + fakestoreApi has failed a few times in the proces,
+// Loader element for ux + fakestoreApi has failed a few times in the proces,
 //so to keep the user informed we implemented this on top of our catch-error message
-const loader = document.getElementById("loader-element");
-
-//for shifting the headings content
+const loader = document.querySelector("svg#loader-element");
+// For shifting the headings content
 const heading = document.querySelector("h1.heading");
-//for toggling the filter buttons
+// For toggling the filter buttons
 /* this is the top level filter btn */
-const toggleButton = document.getElementById("toggle-filter-buttons");
+const toggleButton = document.querySelector("button#toggle-filter-buttons");
 /* this is the div that holds the filter btns */
-const filterButtons = document.getElementById("filter-buttons-div");
+const filterButtons = document.querySelector("div#filter-buttons-div");
 /* this is the actual filter btns, naming mistake.... */
 const theActualFilterButtons = document.querySelectorAll(
   ".filter-buttons button"
 );
+/* this is the buttons w. categories in the footer */
 const footerCategoryLinks = document.querySelectorAll("button.category-link");
-
-//we want to show all products as default
+// ################################################# //
+/* BACK TO TOP TEST */
+const showOnPx = 100;
+const backToTopButton = document.querySelector("button.back-to-top");
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
+document.addEventListener("scroll", () => {
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hiddenBtn");
+  } else {
+    backToTopButton.classList.add("hiddenBtn");
+  }
+});
+// Actually going back to top
+const goToTop = () => {
+  document.body.scrollIntoView({
+    behavior: "smooth",
+  });
+};
+backToTopButton.addEventListener("click", goToTop);
+// ################################################# //
+// We want to show all products as default
 fetchAllProducts();
-//we have a button to toggle the 4 filter buttons
+// We have a button to toggle the 4 filter buttons
 //they are hidden as default
 toggleFilterButtons();
 
-//TEST
+// Category footer link clicked in singleview
 const urlParamsCategory = new URLSearchParams(window.location.search);
 const clickedCategoryFromSingleviewFooter = urlParamsCategory.get("category");
+
+// Fetch the clicked from the footer category
 async function footerCategoryClickedFromSingleview() {
   window.scrollTo(0, 0);
   try {
@@ -64,11 +87,11 @@ async function footerCategoryClickedFromSingleview() {
     loader.style.display = "none";
   }
 }
-//only call the function if the paramter is 'active' / relevant,
-//otherwise it conflicts with the show products logic
+// Only call the function ^ if the paramter is 'active' / relevant
 if (clickedCategoryFromSingleviewFooter) {
   footerCategoryClickedFromSingleview();
 }
+// Function that takes care of footer category links clicked from productview page
 function footerCategoryLinksClickEvent() {
   footerCategoryLinks.forEach((categoryLink) =>
     categoryLink.addEventListener("click", filterProductsByCategory)
@@ -76,6 +99,8 @@ function footerCategoryLinksClickEvent() {
 }
 footerCategoryLinksClickEvent();
 
+// Toggling classes on the filter buttons depending on what
+// is going on
 function toggleFilterButtons() {
   toggleButton.addEventListener("click", () => {
     filterButtons.classList.toggle("hidden");
@@ -96,7 +121,7 @@ function toggleFilterButtons() {
   //we want to establish click events for our 'filter' buttons
   filterButtonClickEvents();
 }
-//when one of the category buttons is clicked, the filterProductsByCategory function gets called
+// When one of the category buttons is clicked, the filterProductsByCategory function gets called
 //when the 'all products' button is clicked, the default fetchAllProducts function gets called
 function filterButtonClickEvents() {
   theActualFilterButtons.forEach((filterButton) =>
@@ -107,7 +132,7 @@ function filterButtonClickEvents() {
     .addEventListener("click", fetchAllProducts);
 }
 
-//in this function we fetch the products that match the querystring
+// In this function we fetch the products that match the querystring
 //and the 'this.dataset.category' refelcts one of the category filter buttons
 //if the user clicks on 'electronics', 'this' is electronics
 async function filterProductsByCategory() {
@@ -137,7 +162,7 @@ async function filterProductsByCategory() {
   }
 }
 
-//our default fetch function, output is all products
+// Our default fetch function, output is all products
 async function fetchAllProducts() {
   /* active button logic under here, and till the hastag seperation */
   toggleButton.classList.remove("active-button");
@@ -168,109 +193,159 @@ async function fetchAllProducts() {
   }
 }
 
-//THIS IS OUR DEFAULT SHOW PRODUCTS FUNCTION, SHOWS ALL PRODUCTS
+// THIS IS OUR DEFAULT SHOW PRODUCTS FUNCTION, SHOWS ALL PRODUCTS
 function showProducts(products) {
   /*   console.log(products); */
-  //show data in HTMl by using the template and clone the data
-  //this is the section we created in our HTML to append the data
+  // Show data in HTMl by using the template and clone the data
+  // productContainer is the section we created in our HTML to append the data
   const productContainer = document.querySelector("section#products");
-  //this is the HTML-template (structured the way we want to display the data)
+  // ProductTemplate is the HTML-template (structured the way we want to display the data)
   const productTemplate = document.querySelector("template#products-template");
-  //making sure the container is empty and ready to clone data
+  // Making sure the container is empty and ready to clone data
   productContainer.textContent = "";
 
+  // Heading textContent
   heading.textContent = "All products";
 
+  // Looping through all the products
   products.forEach((product) => {
-    //making the first letter capitalized, others to lowercase
+    // Making the first letter in the title capitalized,
+    // rest to lowercase
     let modifiedProductTitle =
       product.title.charAt(0).toUpperCase() +
       product.title.slice(1).toLowerCase();
-    //cloning
+    // Cloning
     const cloneProductData = productTemplate.cloneNode(true).content;
-    //placing the products content in our template fields
-    //img
+    // We put this in a const to have a clearer view later on when we use it
+    const addToCartButton = cloneProductData.querySelector(
+      "button.add-to-cart-button"
+    );
+    /* #### Placing the products content in our template fields #### */
+    cloneProductData
+      .querySelector("article.product")
+      .setAttribute("data-product-id", product.id);
+    // Product img
     cloneProductData.querySelector(
       "div.product-img-wrapper"
     ).style.backgroundImage = `url('${product.image}')`;
-    //product title (if longer than 10 chars we cut it)
+    // Product title (if longer than 10 chars we cut it)
     cloneProductData.querySelector("h2.product-title").textContent =
       modifiedProductTitle.length > 10
         ? modifiedProductTitle.slice(0, 10) + ".."
         : modifiedProductTitle;
-    /* test */
-    //accessibility on product title for screenreader, due to us cutting the length
+    // Accessibility on product title for screenreader, due to us cutting the length
     cloneProductData
       .querySelector("h2.product-title")
       .setAttribute("aria-label", modifiedProductTitle);
-    //product price
+    // Product price
     cloneProductData.querySelector("p.product-price").textContent =
       product.price + ",-";
-    //click event on the article (so we later can go to singleview)
+    // Click event on the product (so we later can go to singleview)
     cloneProductData
-      .querySelector("article.product")
+      .querySelector("div.clickable-container")
       .addEventListener("click", () => showSingleProduct(product));
-    //append content to section in HTML
+
+    /* #### RELATED TO CART FUNCTIONALITY #### */
+    addToCartButton.setAttribute("data-product-id", product.id);
+    addToCartButton.addEventListener("click", (event) =>
+      addProductToCart(product, event)
+    );
+
+    // Append content to section in HTML
     productContainer.appendChild(cloneProductData);
   });
 }
+// Function that handles adding to cart
+// and stores the data + sends out an event that contains
+// the updated cart
+function addProductToCart(product, event) {
+  /*   console.log(product); */
+  // Get the exisiting cart
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-//THIS IS OUR SECOND SHOW PRODUCTS FUNCTION, OUTPUTS PRODUCTS TO SPECIFIC CATEGORY
-//we use the same template
+  // Add the new product to the cart
+  cart.push(product);
+
+  // Save the updated cart in localStorage
+  localStorage.setItem("cart", JSON.stringify(cart));
+
+  // Sending out an event to make clear the cart has been updated
+  const updateCartEvent = new CustomEvent("updateCartEvent", { detail: cart });
+  window.dispatchEvent(updateCartEvent);
+
+  // Find the relevant button and add the class
+  const addToCartButton = event.currentTarget;
+  addToCartButton.classList.add("add-product-success");
+
+  document.querySelector("circle.red-dot-on-cart").classList.remove("hidden");
+}
+
+// THIS IS OUR SECOND SHOW PRODUCTS FUNCTION, OUTPUTS PRODUCTS TO SPECIFIC CATEGORY
+// We use the same template
 function showCategoryProducts(categoryProducts) {
   /*   console.log("I am the shoW categories function"); */
-  //this is the section we created in our HTML to append the data
+  // productContainer is the section we created in our HTML to append the data
   const productContainer = document.querySelector("section#products");
-  //this is the HTML-template (structured the way we want to display the data)
+  // ProductTemplate is the HTML-template (structured the way we want to display the data)
   const productTemplate = document.querySelector("template#products-template");
-  //making sure the container is empty and ready to clone data
+  // Making sure the container is empty and ready to clone data
   productContainer.textContent = "";
 
   categoryProducts.forEach((product) => {
-    //modifying the title
+    // Modifying the title
     let modifiedProductTitle =
       product.title.charAt(0).toUpperCase() +
       product.title.slice(1).toLowerCase();
-    //Setting the headings content to the chosen categorys name + capitalizing the first letter
-    //we use charAt(0) to get the first letter, then we capitalize it, and then we
-    //take the rest of the sentence by taking it from position 1 and further
+    // Setting the headings content to the chosen categorys name + capitalizing the first letter
+    // we use charAt(0) to get the first letter, then we capitalize it, and then we
+    // take the rest of the sentence by taking it from position 1 and further
     heading.textContent =
       product.category.charAt(0).toUpperCase() + product.category.substring(1);
-    //cloning
+    // Cloning
     const cloneProductData = productTemplate.cloneNode(true).content;
-    //placing the products content in our template fields
-    //img
+    // We put this in a const to have a clearer view later on when we use it
+    const addToCartButton = cloneProductData.querySelector(
+      "button.add-to-cart-button"
+    );
+    // #### Placing the products content in our template fields ####
+    // Product img
     /*    cloneProductData.querySelector("img.product-img").src = product.image; */
     cloneProductData.querySelector(
       "div.product-img-wrapper"
     ).style.backgroundImage = `url('${product.image}')`;
-    //product title (if longer than 10 chars we cut it)
+    // Product title (if longer than 10 chars we cut it)
     cloneProductData.querySelector("h2.product-title").textContent =
       modifiedProductTitle.length > 10
         ? modifiedProductTitle.slice(0, 10) + ".."
         : modifiedProductTitle;
-    /* test */
-    //accessibility on product title for screenreader, due to us cutting the length
+    // Accessibility on product title for screenreader, due to us cutting the length
     cloneProductData
       .querySelector("h2.product-title")
       .setAttribute("aria-label", modifiedProductTitle);
-    //product price
+    // Product price
     cloneProductData.querySelector("p.product-price").textContent =
       product.price + ",-";
-
+    // Click event to go to singleview page
     cloneProductData
-      .querySelector("article.product")
+      .querySelector("div.clickable-container")
       .addEventListener("click", () => showSingleProduct(product));
-    //append content to section in HTML
+
+    /* #### RELATED TO CART FUNCTIONALITY #### */
+    addToCartButton.setAttribute("data-product-id", product.id);
+    addToCartButton.addEventListener("click", (event) =>
+      addProductToCart(product, event)
+    );
+    // Append content to section in HTML
     productContainer.appendChild(cloneProductData);
   });
 }
 
+// Send user to singleview for the specific product
 function showSingleProduct(product) {
   location.href = `singleview.html?id=${product.id}`;
 }
 
-//show user error message (from the catch code)
+// Show user error message (from the catch code)
 function informUserOfError(message) {
   document.querySelector("h1.heading").textContent = "";
   document.querySelector("h1.heading").textContent = message;
